8acf79111e20540ac5d6a8ec2a0b4c9f4a760b77
na_th_an <na_th_an@hotmail.com>
2021-09-29 10:06:32 +0200

    poo

--- src/dev/loader/loadercpc.asm
+++ src/dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 13677
-	ld      de, 13677
+	ld      ix, $BA00 - 13683
+	ld      de, 13683
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 13677
+	ld      hl, $BA00 - 13683
 	ld      de, $0400
 	call    dzx7_standard
 
