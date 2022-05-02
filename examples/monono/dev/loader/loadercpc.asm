ac0d70f4c9bc13aa3d0468fdafcddea8b3002809
na_th_an <na_th_an@hotmail.com>
2022-05-02 16:22:33 +0200

    Made some further changes and made backend even faster

--- examples/monono/dev/loader/loadercpc.asm
+++ examples/monono/dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 13411
-	ld      de, 13411
+	ld      ix, $BA00 - 13357
+	ld      de, 13357
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 13411
+	ld      hl, $BA00 - 13357
 	ld      de, $0400
 	call    dzx7_standard
 
