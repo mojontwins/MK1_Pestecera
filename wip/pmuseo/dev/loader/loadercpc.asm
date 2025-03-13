e7c32d1e1312083189a7686a79883377efdd4f8c
Angel <angel.lo@ril.es>
2025-03-13 22:21:52 +0100

    Arreglar objeto y meter música

--- wip/pmuseo/dev/loader/loadercpc.asm
+++ wip/pmuseo/dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 15726
-	ld      de, 15726
+	ld      ix, $BA00 - 16019
+	ld      de, 16019
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 15726
+	ld      hl, $BA00 - 16019
 	ld      de, $0400
 	call    dzx7_standard
 
