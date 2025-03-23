aa7f99485d40ef3b8da5499795ea018950dc797a
Angel <angel.lo@ril.es>
2025-03-23 19:06:46 +0100

    Corregir error, revisar musica al salir

--- wip/pmuseo/dev/loader/loadercpc.asm
+++ wip/pmuseo/dev/loader/loadercpc.asm
@@ -30,13 +30,13 @@ org $BA00
 
 ; Load binary
 
-	ld      ix, $BA00 - 16832
-	ld      de, 16832
+	ld      ix, $BA00 - 16839
+	ld      de, 16839
 	call    cpct_miniload_asm
 
 ; Depack binary
 
-	ld      hl, $BA00 - 16832
+	ld      hl, $BA00 - 16839
 	ld      de, $0400
 	call    dzx7_standard
 
